58dc6bf21d1a59c5af6f4bbd2bdd5f4116617581
alkonsafe <philblakeschool@outlook.com>
2025-08-16 20:48:21 -0500

    Replace with (slightly) edited GUI kernel

--- Kernel.cs
+++ Kernel.cs
@@ -1,24 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Cosmos.System.Graphics;
+using System;
+using System.Drawing;
 using Sys = Cosmos.System;
 
-namespace CosmosKernel2
+namespace GraphicTest
 {
     public class Kernel : Sys.Kernel
     {
+        Canvas canvas;
+
+        private readonly Bitmap bitmap = new Bitmap(10, 10,
+                new byte[] { 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0,
+                    255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255,
+                    0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255,
+                    0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 23, 59, 88, 255,
+                    23, 59, 88, 255, 0, 255, 243, 255, 0, 255, 243, 255, 23, 59, 88, 255, 23, 59, 88, 255, 0, 255, 243, 255, 0,
+                    255, 243, 255, 0, 255, 243, 255, 23, 59, 88, 255, 153, 57, 12, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255,
+                    243, 255, 0, 255, 243, 255, 153, 57, 12, 255, 23, 59, 88, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243,
+                    255, 0, 255, 243, 255, 0, 255, 243, 255, 72, 72, 72, 255, 72, 72, 72, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0,
+                    255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 72, 72,
+                    72, 255, 72, 72, 72, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255,
+                    10, 66, 148, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255,
+                    243, 255, 10, 66, 148, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 10, 66, 148, 255, 10, 66, 148, 255,
+                    10, 66, 148, 255, 10, 66, 148, 255, 10, 66, 148, 255, 10, 66, 148, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255,
+                    243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 10, 66, 148, 255, 10, 66, 148, 255, 10, 66, 148, 255, 10, 66, 148,
+                    255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255,
+                    0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, 0, 255, 243, 255, }, ColorDepth.ColorDepth32);
 
         protected override void BeforeRun()
         {
-            Console.WriteLine("Alkon booted successfully. Type a line of text to get it echoed back.");
+            // If all works correctly you should not really see this :-)
+            Console.WriteLine("Cosmos booted successfully. Let's go in Graphical Mode");
+
+            
+
+            /*
+            You don't have to specify the Mode, but here we do to show that you can.
+            To not specify the Mode and pick the best one, use:
+            canvas = FullScreenCanvas.GetFullScreenCanvas();
+            */
+            canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(640, 480, ColorDepth.ColorDepth32));
+
+            // This will clear the canvas with the specified color.
+            canvas.Clear(Color.Blue);
+
         }
+
+        Pen tryin;
 
         protected override void Run()
         {
-            Console.Write("Input: ");
-            var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+            
+            try
+            {
+                
+
+                // A bitmap
+                canvas.DrawImage(bitmap, 0, 0);
+                canvas.Display(); // Required for something to be displayed when using a double buffered driver
+
+                Console.ReadKey();
+                Sys.Power.Shutdown();
+            }
+            catch (Exception e)
+            {
+                Sys.Power.Shutdown();
+            }
         }
     }
 }
